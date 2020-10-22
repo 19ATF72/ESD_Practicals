4e8819f5ee83b7987e7d95b8930baec43371a84c
19ATF77 <micah.hobby@gmail.com>
2020-10-22 15:01:42 +0100

    Initial Project Commit

---
+++ src/esd_practicals/ESD_Practicals.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esd_practicals;
+
+/**
+ *
+ * @author atf1972
+ */
+public class ESD_Practicals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
